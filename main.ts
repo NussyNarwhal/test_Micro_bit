f46f9afc59239dd1b7fe00be7a7646c234ff3f77
NussyNarwhal <98988961+NussyNarwhal@users.noreply.github.com>
2022-08-24 12:44:27 -0600

    made a counter

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
-basic.forever(function () {
-    basic.showIcon(IconNames.Butterfly)
+input.onButtonPressed(Button.A, function () {
+	
 })
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    count = 0
+})
+input.onGesture(Gesture.Shake, function () {
+    count += 1
+    basic.showNumber(count)
+})
+let count = 0
+basic.showIcon(IconNames.Yes)
+basic.pause(100)
+basic.clearScreen()
+count = 0
+basic.showNumber(count)
