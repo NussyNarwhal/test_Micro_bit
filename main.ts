b54e4bc7bc6fbbe7d41e5ae7cc75271076d62f03
NussyNarwhal <98988961+NussyNarwhal@users.noreply.github.com>
2022-08-24 12:59:10 -0600

    using black cord clips and black cord clips

--- main.ts
+++ main.ts
@@ -1,17 +1,8 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
-    count = 0
-})
-input.onGesture(Gesture.Shake, function () {
+input.onPinPressed(TouchPin.P0, function () {
     count += 1
     basic.showNumber(count)
 })
 let count = 0
-basic.showIcon(IconNames.Yes)
-basic.pause(100)
-basic.clearScreen()
+pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
 count = 0
 basic.showNumber(count)
